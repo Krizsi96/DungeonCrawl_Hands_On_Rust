76890d2ff0ef21711d4fcc0751d5618438dfcdd6
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2024-09-02 22:10:19 +0200

    FEAT:: navigating the map

---
+++ src/systems/end_turn.rs
@@ -0,0 +1,12 @@
+use crate::prelude::*;
+
+#[system]
+pub fn end_turn(#[resource] turn_state: &mut TurnState) {
+    let new_state = match turn_state {
+        TurnState::AwaititngInput => return,
+        TurnState::PlayerTurn => TurnState::MonsterTurn,
+        TurnState::MonsterTurn => TurnState::AwaititngInput,
+    };
+    
+    *turn_state = new_state;
+}
